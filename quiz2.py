eccec4ef59063d4fbc2e8288e72dd27fe9fe63e1
charithakishan <59818319+charithakishan@users.noreply.github.com>
2021-10-02 21:44:35 +0530

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,23 @@
+# print(100/25)
+# print("p" in "Python")
+# print(9//2)
+
+# if (9 < 0) and (0 < -9):
+# 	print("hello")
+# elif (9 > 0) or False:
+# 	print("good")
+# else:
+# 	print("bad")
+
+# print(not(3>4))
+# print(not(1&1))
+
+name='ankit'
+age= 10
+if age == 10:
+ if name == 'ankit':
+ 	print("found")
+ else:
+ 	print("no amith")
+else:
+ 	print("no one age with 10")
